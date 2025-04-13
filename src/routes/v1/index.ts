db170f50ebb6aab1254c6cd2e51e802e2bf787b3
Berker Demirer <berkerdemirer@Berkers-MacBook-Pro.local>
2025-04-13 22:56:33 +0300

    add openapi and scalar for docs

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,4 +1,6 @@
+import { Scalar } from "@scalar/hono-api-reference";
 import { Hono } from "hono";
+import { openAPISpecs } from "hono-openapi";
 
 import { user } from "./user.js";
 
@@ -10,6 +12,28 @@ v1.get("/health", (c) => {
     timestamp: new Date().toISOString(),
   });
 });
+
+v1.get(
+  "/openapi",
+  openAPISpecs(v1, {
+    documentation: {
+      info: {
+        description: "Boilerplate API",
+        title: "Hono API",
+        version: "1.0.0",
+      },
+      servers: [{ description: "Local Server", url: "http://localhost:3000" }],
+    },
+  }),
+);
+
+v1.get(
+  "/docs",
+  Scalar({
+    theme: "saturn",
+    url: "/api/v1/openapi",
+  }),
+);
 
 v1.route("/users", user);
 
--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,9 +1,38 @@
 import { getUsers } from "#src/controllers/users.js";
 import { protect } from "#src/middlewares/auth.js";
 import { Hono } from "hono";
-
+import { describeRoute } from "hono-openapi";
+import { resolver } from "hono-openapi/zod";
+import { z } from "zod";
 const user = new Hono();
 
-user.get("/", protect, getUsers);
+const usersResponseSchema = z.object({
+  users: z.array(
+    z.object({
+      createdAt: z.string(),
+      email: z.string(),
+      id: z.string(),
+      name: z.string(),
+      updatedAt: z.string(),
+    }),
+  ),
+});
+
+user.get(
+  "/",
+  describeRoute({
+    description: "Get all users",
+    responses: {
+      200: {
+        content: {
+          "application/json": { schema: resolver(usersResponseSchema) },
+        },
+        description: "Successful response",
+      },
+    },
+  }),
+  protect,
+  getUsers,
+);
 
 export { user };
